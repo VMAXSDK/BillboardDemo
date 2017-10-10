aab35525346dfd2f8a120076a93ae30d3f4d24f3
prathameshs <prathamesh.s@vmax.com>
2017-10-10 15:22:36 +0530

    3.7.8 update

--- app/src/main/java/com/vmax/demo/billboard/MainActivity.java
+++ app/src/main/java/com/vmax/demo/billboard/MainActivity.java
@@ -2,7 +2,6 @@ package com.vmax.demo.billboard;
 
 
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
